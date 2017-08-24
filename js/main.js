90b5736476de8e0a2e2aee4f4891546a3edf899f
Karoline <karolinedalencar@gmail.com>
2017-08-24 12:16:32 -0300

    finish lesson 2 - making your mark

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 var map;
 
 function initMap() {
+
+  // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: 40.7413549, lng: -73.9980244 },
     zoom: 13
   });
+
+  // Constructor creates a new Marker - position and map are required.
+  var tribeca = { lat: 40.719526, lng: -74.0089934 };
+  var marker = new google.maps.Marker({
+    position: tribeca,
+    map: map,
+    title: 'First Marker!'
+  })
+
 }
